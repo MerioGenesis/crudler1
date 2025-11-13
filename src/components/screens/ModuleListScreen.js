170d10358662519a1ee46c1b6fba69251ca0694b
Mariusz <mariopif20@hotmail.com>
2025-11-13 13:55:21 +0000

    Activity #13
    Introduced API get method to retrieve data from the softwarehub server.

--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -1,19 +1,29 @@
-import { useState } from 'react';
-import { LogBox, StyleSheet } from 'react-native';
+import { useState, useEffect } from 'react';
+import { LogBox, Text, StyleSheet } from 'react-native';
+import API from '../API/API.js';
 import Screen from '../layout/Screen';
 import Icons from '../UI/Icons.js';
 import { Button, ButtonTray } from '../UI/Button.js';
 
 import ModuleList from '../entity/modules/ModuleList.js';
-import initialModules from '../../data/modules.js';
+
 
 
 const ModuleListScreen = ({ navigation }) => {
   //Initialisations -----------------
   LogBox.ignoreLogs(['Non-serializable values were found in the navigation state']);
+  const modulesEndpoint = 'https://softwarehub.uk/unibase/api/modules';
   // State --------------------------
-  const [modules, setModules] = useState(initialModules)
+  const [modules, setModules] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
+  const loadModules = async (endpoint) =>{
+    const response = await API.get(endpoint);
+    setIsLoading(false);
+    if(response.isSuccess) setModules(response.result);
+  }
+  useEffect(() => {loadModules(modulesEndpoint)}, []);
+  
   // Handlers -----------------------
   const handleDelete = (module) => setModules(modules.filter((item) => item.ModuleID !== module.ModuleID));
   const handleAdd = (module) => setModules([...modules, module]);
@@ -46,6 +56,10 @@ const ModuleListScreen = ({ navigation }) => {
       <ButtonTray>
         <Button label="Add" icon={<Icons.Add />} onClick={gotoAddScreen} />
       </ButtonTray>
+      {
+        isLoading &&<Text>Loading records...</Text>
+      }
+      <Text>Loading records...</Text>
       <ModuleList modules={modules} onSelect={gotoViewScreen} />
     </Screen>
   );
